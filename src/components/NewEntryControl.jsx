7551fa7566eaa17f252dbc22c6ba676ca84c1ac4
Lara Bjork <lara.m.bjork@gmail.com>
2020-03-11 15:59:16 -0700

    add unsuccessful attempt to change new entry controller to show med milestone vs journal

--- src/components/NewEntryControl.jsx
+++ src/components/NewEntryControl.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NewGeneralJournalRecordForm from './NewGeneralJournalRecordForm';
+import NewMedHistoryMilestoneForm from './NewMedHistoryMilestoneForm';
 import Dashboard from './Dashboard';
 import PropTypes from 'prop-types';
 
@@ -8,19 +9,26 @@ class NewEntryControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      journalFormVisibleOnPage: false
+      journalFormVisibleOnPage: false,
+      medMilestoneFormVisibleOnPage: false
     };
     this.handleStartNewJournalEntry = this.handleStartNewJournalEntry.bind(this);
+    this.handleStartNewMedMilestone = this.handleStartNewMedMilestone.bind(this);
   }
 
   handleStartNewJournalEntry(){
     this.setState({journalFormVisibleOnPage: true});
+  }
+  handleStartNewMedMilestone(){
+    this.setState({medMilestoneFormVisibleOnPage: true});
   }
 
   render(){
     let currentlyVisibleContent = null;
     if (this.state.journalFormVisibleOnPage){
       currentlyVisibleContent = <NewGeneralJournalRecordForm onNewJournalEntryCreation={this.props.onNewJournalEntryCreation}/>
+    } elseif (this.state.medMilestoneFormVisibleOnPage){
+     currentlyVisibleContent = <NewMedHistoryMilestoneForm onNewMedMilestoneEntryCreation={this.props.onNewMedMilestoneEntryCreation}/>
     } else {
       currentlyVisibleContent = <Dashboard onStartNewJournalEntry={this.handleStartNewJournalEntry}/>;
     }
@@ -33,7 +41,8 @@ class NewEntryControl extends React.Component {
 }
 
 NewEntryControl.propTypes = {
-  onNewJournalEntryCreation: PropTypes.func
+  onNewJournalEntryCreation: PropTypes.func,
+  onNewMedMilestoneEntryCreation: PropTypes.func
 }
 
 export default NewEntryControl;
